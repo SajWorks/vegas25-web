85bf1d5a8a08d563b20c636d0cc7f6036c52bc84
Matt Boyd <mboyd1993@gmail.com>
2025-07-08 11:42:25 -0500

    Changed from data endpoint to game_state endpoint

--- api.js
+++ api.js
@@ -1,18 +1,20 @@
 const BASE_URL = 'https://amazing-crane-ghastly.ngrok-free.app';
 const DATA_PATH = '/api/data';
 const PLAY_PATH = '/api/play';
+const GAME_STATE_PATH = '/api/game_state';
 const DATA_URL = BASE_URL + DATA_PATH;
 const PLAY_URL = BASE_URL + PLAY_PATH;
+const GAME_STATE_URL = BASE_URL + GAME_STATE_PATH;
 
 async function fetchData() {
     try {
-    const res = await fetch(DATA_URL, {
+    const res = await fetch(GAME_STATE_URL, {
         headers: {
             'ngrok-skip-browser-warning': 'true'
         }
     });
     const data = await res.json();
-    document.getElementById('tempF').textContent = data.tempF ?? '--';
+    document.getElementById('tempF').textContent = data.state ?? '--';
     document.getElementById('tempC').textContent = data.tempC ?? '--';
     document.getElementById('hum').textContent = data.humidity ?? '--';
     } catch (err) {
